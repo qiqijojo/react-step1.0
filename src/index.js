b2a7b7abe999109710ce58dd4714dc3dcd486191
王齐 qi.wang01 <qi.wang01@weimob.com>
2018-05-30 19:33:19 +0800

    添加.eslint .babelrc react

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 
-function component() {
-    var element = document.createElement('div');
+// function component() {
+//     var element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack1.0'], ' ');
+//     element.innerHTML = _.join(['啦啦啦', '删除dddd了'], ' ');
 
-    return element;
+//     return element;
+// }
+
+// document.body.appendChild(component());
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Router, Route } from 'react-router-dom';
+// import {  } from ''
+
+// const App = () => (
+//     <div>啊哈哈哈哈</div>
+// )
+class App extends React.Component{
+    render() {
+        return (
+            <div>啊哈哈哈sss哈</div>
+        )
+    }
 }
-
-document.body.appendChild(component());
+ReactDom.render(
+    <App/>,
+     document.getElementById('app'),
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,37 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index.js',
-    devServer: {
-        contentBase: './dist'
+    entry: {
+        app: path.resolve(__dirname, './src/index.js')
     },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    mode: 'development',
     output: {
-        filename: 'bundle.js',
+        filename: '[name]_[hash:8].bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './public/index.html'), // src文件
+            filename: 'index.html'// dist文件
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: require.resolve('babel-loader'),
+                exclude: /node-modules/
+            }
+        ]
     }
 };
