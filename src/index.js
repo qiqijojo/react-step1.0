926e18130de2d2041075d0b1a910b3d7608ba001
王齐 qi.wang01 <qi.wang01@weimob.com>
2018-05-31 15:23:51 +0800

    ok

--- src/index.js
+++ src/index.js
@@ -1,30 +1,19 @@
-// import _ from 'lodash';
-
-// function component() {
-//     var element = document.createElement('div');
-
-//     element.innerHTML = _.join(['啦啦啦', '删除dddd了'], ' ');
-
-//     return element;
-// }
-
-// document.body.appendChild(component());
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Router, Route } from 'react-router-dom';
-// import {  } from ''
 
-// const App = () => (
-//     <div>啊哈哈哈哈</div>
-// )
-class App extends React.Component{
+class App extends React.Component {
     render() {
         return (
-            <div>啊哈哈哈sss哈</div>
-        )
+            <div>
+                啊哈哈哈张艺兴
+                <h2>dddfff</h2>
+            </div>
+        );
     }
 }
 ReactDom.render(
-    <App/>,
-     document.getElementById('app'),
-)
+    <App />,
+    document.getElementById('app'),
+);
+
